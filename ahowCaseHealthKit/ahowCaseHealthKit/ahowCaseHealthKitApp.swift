ebc278d680079dcc1d44df7d505828a61771f2c3
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-05-16 16:44:11 +0200

    Initial project creation

---
+++ ahowCaseHealthKit/ahowCaseHealthKit/ahowCaseHealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ahowCaseHealthKitApp.swift
+//  ahowCaseHealthKit
+//
+//  Created by Arthur Nsereko Kahwa on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ahowCaseHealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
