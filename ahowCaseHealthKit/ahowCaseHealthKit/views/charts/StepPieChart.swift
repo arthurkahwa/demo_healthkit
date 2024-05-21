f03ca523cfb920f51d98643dc710b3228e28c029
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-05-21 14:01:16 +0200

    Copy Average VStack to pie chart

---
+++ ahowCaseHealthKit/ahowCaseHealthKit/views/charts/StepPieChart.swift
@@ -0,0 +1,18 @@
+//
+//  StepPieChart.swift
+//  ahowCaseHealthKit
+//
+//  Created by Arthur Nsereko Kahwa on 5/21/24.
+//
+
+import SwiftUI
+
+struct StepPieChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StepPieChart()
+}
