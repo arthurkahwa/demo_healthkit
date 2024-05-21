514a7fe093f7c90f13fc9050c0fc707ecf837d2a
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-05-21 14:03:14 +0200

    Copy averages to step pie chart

--- ahowCaseHealthKit/ahowCaseHealthKit/views/charts/StepPieChart.swift
+++ ahowCaseHealthKit/ahowCaseHealthKit/views/charts/StepPieChart.swift
@@ -6,10 +6,29 @@
 //
 
 import SwiftUI
+import Charts
 
 struct StepPieChart: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) { // Overall chart card
+            VStack(alignment: .leading) {
+                Label("Averages", systemImage: "calendar")
+                    .font(.title3.bold())
+                    .foregroundStyle(.pink)
+                
+                Text("last 28 days")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+                
+            }
+            .padding(.bottom, 12)
+            
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundStyle(.secondary)
+                .frame(height: 240)
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
     }
 }
 
