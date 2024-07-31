6334d776dab330cad2e8432f086b69bab864f586
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-07-31 16:17:22 +0200

    Update documentation and create release 0.9

--- ahowCaseHealthKit/ahowCaseHealthKit/enum/StepTrackerError.swift
+++ ahowCaseHealthKit/ahowCaseHealthKit/enum/StepTrackerError.swift
@@ -7,9 +7,35 @@
 
 import Foundation
 
-enum StepTrackerError: Error {
+enum StepTrackerError: LocalizedError {
     case authNotDetermined
     case sharingDenied(quantityType: String)
     case noData
     case unableToCompleteRequest
+    
+    var errorDescription: String? {
+        switch self {
+        case .authNotDetermined:
+            "Need access to health data"
+        case .sharingDenied(quantityType: let quantityType):
+            "No write access"
+        case .noData:
+            "No data available"
+        case .unableToCompleteRequest:
+            "Unable to complete request"
+        }
+    }
+    
+    var failureReason: String {
+        switch self {
+        case .authNotDetermined:
+            "Give access to health data"
+        case .sharingDenied(quantityType: let quantityType):
+            "Access denied to to \(quantityType) data"
+        case .noData:
+            "No data for this health tatistic"
+        case .unableToCompleteRequest:
+            "Cannot complete request. Try again later."
+        }
+    }
 }
